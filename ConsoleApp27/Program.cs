d650ee5fbe9af214c4c7c26b45dc39fc81c623d5
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-02-15 10:15:03 +0100

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ditt namn.");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("Skriv ditt efternamn.");
+            string ord2 = Console.ReadLine();
+
+            if (ord1.CompareTo(ord2) < 0) 
+
+                Console.WriteLine("Ditt förnamn kommer först i bokstavsordning");
+
+            else
+                Console.WriteLine("Ditt efternamn kommer först i bokstavsordning ");
+            
+        }
+    }
+}
